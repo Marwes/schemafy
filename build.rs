2a96f254732e428959652600b6ecb83c5f61fdfb
Markus <marwes91@gmail.com>
2017-08-11 17:57:26 +0200

    chore: Update snapshot

--- build.rs
+++ build.rs
@@ -13,12 +13,14 @@ fn main() {
     let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
 
-    let output = schemafy_snapshot::generate(Some("Schema"), &input).unwrap();
+    let output = schemafy_snapshot::GenerateBuilder {
+        root_name: Some("Schema"),
+        schemafy_path: "::",
+    }.build(&input).unwrap();
     let dst = Path::new("src/schema.rs");
 
     let mut file = File::create(dst).unwrap();
     file.write_all(br#"
-    use one_or_many;
     use serde_json;
     "#)
         .unwrap();
